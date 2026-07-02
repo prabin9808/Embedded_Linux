209d5bfe5a7cc846ad025ceed0c641b9bf02d3bc
Prabin kumar <39507514+prabin9808@users.noreply.github.com>
2026-07-02 22:21:52 +0530

    002_pcd_driver file get added

---
+++ pcd.c
@@ -0,0 +1,30 @@
+#include<linux/module.h>
+#include<linux/fs.h>
+
+#define DEV_MEM_SIZE 512
+
+char device_buffer[DEV_MEM_SIZE];
+
+dev_t device_number;  // This holds device number
+static int __init pcd_driver_init(void)
+{
+	// 1. Dynamically Allocate a device number
+	alloc_chrdev_region(&device_number, 0, 1, "pcd");
+	
+	// 2.  
+
+	return 0;
+}
+
+static void __exit pcd_driver_cleanup(void)
+{
+
+}
+
+module_init(pcd_driver_init);
+module_exit(pcd_driver_cleanup);
+
+MODULE_LICENSE("GPL");
+//MUDULE_AUTHOR("Prabin Kumar");
+//MODULE_DESCRIPTION("A pseudo character driver");
+

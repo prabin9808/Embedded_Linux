ab160f17a4d50fe6bcd11144f3e771564272df2d
Prabin kumar <39507514+prabin9808@users.noreply.github.com>
2026-07-03 07:24:23 +0530

    Add files via upload

--- pcd.c
+++ pcd.c
@@ -1,17 +1,32 @@
 #include<linux/module.h>
 #include<linux/fs.h>
-
+#include<linux/cdev.h>
 #define DEV_MEM_SIZE 512
 
 char device_buffer[DEV_MEM_SIZE];
 
 dev_t device_number;  // This holds device number
+		      
+// cdev variable
+struct cdev pcd_cdev;
+
+// file operations of the driver
+struct file_operations pcd_fops;
+
+
 static int __init pcd_driver_init(void)
 {
 	// 1. Dynamically Allocate a device number
 	alloc_chrdev_region(&device_number, 0, 1, "pcd");
 	
-	// 2.  
+	// 2. make a char device registratiom with the VFS 
+	// 	initialize the cdev structure with fops
+	
+	cdev_init(&pcd_cdev,&pcd_fops);
+
+	// 3. Register cdev structure with VFS
+	pcd_cdev.owner = THIS_MODULE;
+	cdev_add(&pcd_cdev, device_number, 1);
 
 	return 0;
 }
